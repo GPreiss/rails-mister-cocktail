a0cd9d3de692ab035593b549df8f67cf0e7c1b24
Gabriela Preiss <gabriela.preiss@gmail.com>
2017-11-02 16:05:06 +0000

    add controllrs and routes

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,34 @@
+class DosesController < ApplicationController
+before_action :find_cocktail, only: [:show, :new, :create]
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = @cocktail.doses.create(dose_params)
+    if @dose.save
+      redirect_to cocktail_path(id: @cocktail.id)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktails_path
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+  private
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+end
